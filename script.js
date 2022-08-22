e99fab64ee388cfb140b887c9623117cc0f8194e
Asif0810 <asifaslam1703@gmail.com>
2022-08-22 22:46:34 +0600

    analysist

--- script.js
+++ script.js
@@ -7,15 +7,11 @@ function player_name(input) {
 function naming_Set(input) {
     const listCreat = document.createElement('li');
     listCreat.innerText = input;
-    
-    
-
     const olList = document.getElementById('ol-list');
     olList.appendChild(listCreat);
 
 
 }
-
 
 
 
@@ -25,7 +21,8 @@ document.getElementById('player1_btn ').addEventListener('click', function () {
 
     const player1 = player_name('ronaldo');
 
-    const setName = naming_Set(player1)
+    const setName = naming_Set(player1);
+
 
 
 })
@@ -56,3 +53,65 @@ document.getElementById('player6_btn ').addEventListener('click', function () {
     const player6 = player_name("salah");
     const setName = naming_Set(player6)
 })
+
+function Expense_calculation(input) {
+    const per_Calculate = document.getElementById(input);
+    const per_Calculate_string = per_Calculate.value;
+    const per_Calculate_number = parseFloat(per_Calculate_string);
+    if (isNaN(per_Calculate_number)) {
+        alert("select an amount")
+    } else {
+        per_Calculate.value = '';
+        return per_Calculate_number
+    }
+
+
+
+
+}
+function palyer_text(input) {
+    const player_expense = document.getElementById(input);
+    const player_expense_string = player_expense.innerText;
+    return player_expense
+}
+document.getElementById('perPlayer_calculate').addEventListener('click', function () {
+    const per_Calculate = Expense_calculation('perPlayerInput');
+
+
+    const player_5 = per_Calculate * 5;
+
+    const player_expense = palyer_text('player_expense');
+
+    player_expense.innerText = player_5
+
+
+
+})
+document.getElementById('total_btn').addEventListener('click', function () {
+
+    const coach_input = Expense_calculation('coach_input');
+    const manager_input = Expense_calculation('manager_input')
+
+
+    const player_expense = document.getElementById('player_expense');
+    const player_expense_string = player_expense.innerText;
+    const player_expense_number = parseFloat(player_expense_string);
+
+    const total_value = palyer_text('total_value');
+    const total_calculation = player_expense_number + coach_input + manager_input;
+    total_value.innerText = total_calculation
+
+
+
+
+
+})
+
+
+let buttons = document.getElementsByTagName('button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (event) {
+        event.target.disabled = true;
+    });
+}
+
